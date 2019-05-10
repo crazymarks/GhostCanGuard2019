a44995fef0898f4de4dcca8e912524c42b66fe28
tokyotech-hos <tokyotech-hosyu@4900.co.jp>
2019-05-10 16:25:02 +0900

    gimmic 仮作成projectfileを追加

---
+++ GhostCanGuard2019/Assets/Scripts/LoadTxt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class LoadTxt : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+        
+        var texta = Resources.Load("aaa.txt") as TextAsset;
+        string txt = (Resources.Load("aaa.txt") as TextAsset).text;
+
+    }
+
+    public void LoadasTextAsset()
+    {
+        
+    }
+    
+
+
+}
---
+++ GhostCanGuard2019/Assets/Scripts/gimmick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gimmick : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+
+        Debug.Log("Click");
+    }
+}
