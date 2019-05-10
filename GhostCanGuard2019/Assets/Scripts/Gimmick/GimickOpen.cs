6a252f3fc2ed29404ee2d0429eb0828afd202c6c
tkdhiroki <likeyourbehavior@gmail.com>
2019-05-10 15:09:28 +0900

    playerの移動とギミックの展開、選択を作成 ギミックはまだ仮verです

---
+++ GhostCanGuard2019/Assets/Scripts/Gimmick/GimickOpen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/* 
+ ** 外部スクリプトで呼ぶクラス
+ ** ギミックにマウスクリックが起きた時に
+ ** ギミック効果をUIとして展開する関数をもつ 
+ */
+
+public class GimickOpen : MonoBehaviour
+{
+    // hierarchy上にあるギミック
+    [SerializeField]
+    private GameObject gimmickObject;
+
+    // UIの親の空オブジェ
+    private GameObject gimmickUIParent;
+
+    private void Start()
+    {
+        gimmickUIParent = gimmickObject.transform.GetChild(0).gameObject;
+    }
+
+    /// <summary>
+    /// gimmickの効果を表示、非表示させる
+    /// </summary>
+    /// <param name="onoff">true: 展開  false: 収縮</param>
+    public void GimmickUIsOnOff(bool onoff)
+    {
+        // UI表示
+        gimmickUIParent.SetActive(onoff);
+    }
+}
---
+++ GhostCanGuard2019/Assets/Scripts/Gimmick/GimmicksUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GimmicksUI : MonoBehaviour
+{
+    // playerとの距離が短いときに発動するギミックはtrue
+    //[SerializeField]
+    //private bool toPlayerDistance = false;
+
+    [SerializeField]
+    private EventTrigger eventTrigger;
+
+    private void Start()
+    {
+        /// マウスのポインターを話した時のイベント作成
+        var entryPUP = new EventTrigger.Entry();
+        entryPUP.eventID = EventTriggerType.PointerUp;
+        entryPUP.callback.AddListener((data) => GimmickStart(data));
+        this.eventTrigger.triggers.Add(entryPUP);
+    }
+
+    /// <summary>
+    /// Gimmickの効果発動
+    /// とりあえず今はGimmickの名前をDebugで表示
+    /// </summary>
+    private void GimmickStart(BaseEventData data)
+    {
+        if ( !this.gameObject.activeSelf ) return;
+        Debug.Log(this.gameObject.name);
+    }
+}
