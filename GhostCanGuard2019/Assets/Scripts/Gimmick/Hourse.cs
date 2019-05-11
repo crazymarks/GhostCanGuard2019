048ad1f24310f77ee8b4d87170137fe3e2d4e8cc
mamanju <himazin19970630@gmail.com>
2019-05-11 19:13:38 +0900

    馬60%

--- GhostCanGuard2019/Assets/Scripts/Gimmick/Hourse.cs
+++ GhostCanGuard2019/Assets/Scripts/Gimmick/Hourse.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 public class Hourse : MonoBehaviour
 {
     [SerializeField]
-    private bool IfEnable = true;
-    
+    private bool IfEnable = false;
+    protected bool IfUsed = false;
     //馬の速さ
     [SerializeField]
     private float HourseSpeed = 10.0f;
     //馬の移動時間
     [SerializeField]
     public float HourseMoveTime = 3.0f;
+    public float starttime = 0f;
+    private bool moving=false;
+
     private Rigidbody HourseRB;
 
     public static Hourse instanceHourse;
@@ -21,7 +24,7 @@ public class Hourse : MonoBehaviour
     private bool _HourseMove = true;
     public bool IsHourseMove { get { return _HourseMove; } set { _HourseMove = value; } }
 
-    private float radius = 0.1f;
+    private float radius = 1f;
     [SerializeField]
     private PlayerMove playerMove;
 
@@ -35,65 +38,99 @@ public class Hourse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!IfUsed) return;
+        Move(HourseSpeed, GetOrient());
     }
 
     private void OnTriggerEnter(Collider other)
     {
-       
+        if (!IfUsed&&other.tag=="Player")
+        {
+            IfEnable = true;
+            playerMove.IsPlayerMove = false;
+            StartCoroutine(HourseMove(other.gameObject));
+            IfUsed = true;
+        }
         
     }
-
-
-    private void Move(float speed)
+    private void OnTriggerExit(Collider other)
     {
-        playerMove.IsPlayerMove = false;
-        
-        Vector3 move = Vector3.zero;
+        IfUsed = false;
+        Debug.Log("Reade to Reuse");
+    }
 
-
-
-        // 右
+    private string GetOrient()
+    {// 右
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            move += Vector3.right * speed;
+            return "right";
+        }
+        // 左
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            return "left";
+        }
+        // 上
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            return "up";
+        }
+        // 下
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            return "down";
+        }
+        else return null;
+        
+    }
+    Vector3 LastMove = Vector3.zero;
+    private void Move(float speed,string orient)
+    {
+        // 右
+        if (orient=="right"&&IfEnable)
+        {
+            LastMove = Vector3.right.normalized* speed;
+             
         }
 
         // 左
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (orient == "left" && IfEnable)
         {
-            move += Vector3.left * speed;
+            LastMove = Vector3.left.normalized * speed;
+     
         }
         // 上
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (orient == "up"&& IfEnable)
         {
-            move += Vector3.forward * speed;
+            LastMove = Vector3.forward.normalized * speed;
+          
         }
         // 下
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        if (orient == "down" && IfEnable)
         {
-            move += Vector3.back * speed;
+            LastMove = Vector3.back.normalized * speed;
         }
-        //移動距離を計算する
-        float movedistance = (speed / Mathf.Sqrt(2.0f) * Time.deltaTime);
-        RaycastHit hit;
-        //自身の位置から移動方向に自身の半径+移動距離分の長さのRayを飛ばす
-        if (Physics.Raycast(transform.position, move, out hit, movedistance + radius))
-        {
-            Debug.Log(hit.point);
-            //移動距離をClampして移動距離を制限する
-            movedistance = Mathf.Clamp(movedistance, 0, hit.distance - radius > 0 ? hit.distance - radius : 0);
-        }
-        //transform.positionを変更して移動する
         
-        transform.position += (move.normalized * movedistance);
-        StartCoroutine(HourseMove());
+        Debug.Log(LastMove);
+        transform.position += LastMove * Time.deltaTime;
+        IfEnable = false;
     }
 
-    IEnumerator HourseMove()
+    IEnumerator HourseMove(GameObject player)
     {
-       
+        
+        player.SetActive(false);
+        player.transform.SetParent(this.transform);
+        Debug.Log("Parent Set");
+
+        
         yield return new WaitForSeconds(HourseMoveTime);
+        Move(0,null);
+        player.SetActive(true);
+        this.transform.DetachChildren();
+        playerMove.IsPlayerMove = true;
+        
+        starttime = 0;
     }
 
 
