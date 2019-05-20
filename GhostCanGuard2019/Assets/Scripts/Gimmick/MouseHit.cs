5f0df1c5cb555a2cda60282322b023f15c90fcef
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-05-20 12:06:44 +0900

    ギミックハイライト表示

---
+++ GhostCanGuard2019/Assets/Scripts/Gimmick/MouseHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHit : MonoBehaviour
+{
+    
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+  
+   
+    private void OnMouseOver()
+    {
+        
+        material.SetFloat("_OutlineWidth", 1.3f);
+        Debug.Log(name);
+    }
+    private void OnMouseExit()
+    {
+        material.SetFloat("_OutlineWidth", 1f);
+    }
+
+}
