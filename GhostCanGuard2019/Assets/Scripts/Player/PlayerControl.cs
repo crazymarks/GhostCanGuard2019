77b6fba04fb53f64886cccfb39aa38342af876e6
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-06-14 19:07:12 +0900

    Player移動の仕様の変更 画面遷移の仮作成

---
+++ GhostCanGuard2019/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // playerの速さ
+    
+    public float speed = 5.0f;
+    public float turnSpeed = 0.1f;
+
+    private Rigidbody rb;
+
+    public static PlayerMove instancePM;
+
+    private bool _playerMove = true;
+    public bool IsPlayerMove { get { return _playerMove; } set { _playerMove = value; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move(speed);
+    }
+
+    private void move(float speed)
+    {
+        if (!_playerMove) return;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizontal, 0, vertical).normalized;
+
+
+        rb.velocity = move * speed;
+        
+        if (move!= Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move, Vector3.up), turnSpeed);
+    }
+}
