204bc61a256b66ae4ed8fd5e0c315416eac72986
taoye <714332147@qq.com>
2019-06-21 16:00:50 +0900

    画面遷移の導入

---
+++ GhostCanGuard2019/Assets/Scripts/StartMenu/StartManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public GameObject desPanel;//介绍面板
+
+    //按下开始按钮
+    public void OnStartButtonDown()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //按下介绍按钮
+    public void OnDesButtonDown()
+    {
+        desPanel.SetActive(true);
+    }
+
+    //按下关闭按钮
+    public void OnCloseButtonDown()
+    {
+        desPanel.SetActive(false);
+    }
+
+    //按下退出按钮
+    public void OnQuitButtonDown()
+    {
+        Application.Quit();
+    }
+}
---
+++ GhostCanGuard2019/Assets/Scripts/StartMenu/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //单例模式
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<UIManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public GameObject menuPanel;//菜单面板
+    public GameObject desPanel;//介绍面板
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowMenuPanel();
+        }
+    }
+
+    //显示菜单面板
+    private void ShowMenuPanel()
+    {
+        menuPanel.SetActive(true);
+    }
+
+    //按下是按钮
+    public void OnYButtonDown()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //按下否按钮
+    public void OnNButtonDown()
+    {
+        menuPanel.SetActive(false);
+    }
+
+    //显示介绍面板
+    public void ShowDesPanel(string str)
+    {
+        desPanel.SetActive(true);
+        desPanel.transform.GetChild(0).GetComponent<Text>().text = str;
+    }
+
+    //按下关闭按钮
+    public void OnCloseButtonDown()
+    {
+        desPanel.SetActive(false);
+    }
+}
