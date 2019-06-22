df663d42011820cac3534d5e78235f6e0523a5f3
xiaohaoting <>
2019-06-22 15:40:06 +0900

    script 更新

---
+++ GhostCanGuard2019/Assets/Scripts/StartMenu/StartManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    public GameObject desPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void onDesButtonDown()
+    {
+        desPanel.SetActive(true);
+    }
+
+    public void OnCloseButtonDown()
+    {
+        desPanel.SetActive(false);
+    }
+
+    public void OnQuitButtonDown()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    
+}
