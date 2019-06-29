1c5418235dd7f08ecaed7a7adf62d926e2b12bc0
takada <>
2019-06-29 18:17:50 +0900

    花瓶が割れる処理追加 　花瓶の残りの処理としては花瓶を飛ばすときにplayerに当たってしまわないようにするのと、花瓶を飛ばす際にマウスの方向にplayerを向かせる

--- GhostCanGuard2019/Assets/Scripts/Gimmick/Kabin/KabinGimmick.cs
+++ GhostCanGuard2019/Assets/Scripts/Gimmick/Kabin/KabinGimmick.cs
@@ -1,23 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Rigidbody))]
 public class KabinGimmick : GimmickBase
 {
     private Vector3 throwPos = Vector3.zero;   // 花瓶が飛んでいく場所
+    [SerializeField] private float power = 400f; // 花瓶を押す力
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private float speed = 0.01f;
 
-    [SerializeField]
-    private float power = 400f; // 花瓶を押す力
-    [SerializeField]
-    private GameObject player = null;
-    [SerializeField]
-    private float speed = 0.01f;
+    [SerializeField] private GameObject kabinBreaking = null;    // 割れた花瓶のprefab
+    private Vector3 kabinSpawn = Vector3.zero;                 // 割れた花瓶の出現場所
 
-    private bool kabinSetPos = false;
-
-    private bool kabinFinish = false;
-
+    private bool kabinSetPos = false;       // 指定の場所に花瓶があるかどうか
 
     private Rigidbody rb = null;
 
@@ -28,16 +23,20 @@ public class KabinGimmick : GimmickBase
 
         rb = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        // 初期値のままだったらreturn
+        if (kabinSpawn == Vector3.zero) return;
 
+        KabinBreakingStart();
+    }
     private void KabinGimmickSetup()
     {
         Vector3 playerPosition = player.transform.position;
         playerPosition += player.transform.forward;
 
         KabinToPlayer( playerPosition );
-       
     }
-
     private void KabinGimmickAction()
     {
         KabinGimmickSetup();
@@ -61,16 +60,13 @@ public class KabinGimmick : GimmickBase
         GimmickManager.Instance.ClearGimmick();
         GimmickUIClose();
         ClearGimmickEvent();
-
     }
-
     // ButtonのonClickで呼ぶ関数
     public void ClickUIStart()
     {
         GimmickManager.Instance.SetGimmickAction( () => KabinGimmickAction() );
         GimmickUIsOnOff(false);
     }
-
     private void KabinToPlayer(Vector3 pPos)
     {
         while(transform.position != pPos)
@@ -78,5 +74,27 @@ public class KabinGimmick : GimmickBase
             transform.position = Vector3.MoveTowards(transform.position, pPos, speed);
         }
         kabinSetPos = true;
+    }
+    /// <summary>
+    /// 花瓶が他のオブジェクトに衝突時の地点取得
+    /// </summary>
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 花瓶のギミックが発動してなかったらreturn
+        if (kabinSetPos == false) return;
+        // playerにあたったならreturnする
+        if (collision.gameObject == player) return;
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            // 飛ばした花瓶が接触した地点を取得
+            kabinSpawn = contact.point;
+        }
+    }
+    private void KabinBreakingStart()
+    {
+        Destroy(this.gameObject);
+        kabinSpawn.y = 0.2f;
+        Instantiate(kabinBreaking, kabinSpawn, Quaternion.identity);      
     }
 }
