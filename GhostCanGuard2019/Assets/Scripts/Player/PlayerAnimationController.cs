6baa083934fabe6847d78757d8cf7b386111c738
tkdhiroki <likeyourbehavior@gmail.com>
2019-07-04 00:42:15 +0900

    PlayerのAnimation完成。あとはPlayerのスクリプトに組み込むだけです

--- GhostCanGuard2019/Assets/Scripts/Player/PlayerAnimationController.cs
+++ GhostCanGuard2019/Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,20 +9,27 @@ public enum SetPAnimator
     Run,
     Hold,
     Push,
-    Reset
+    Stop
 }
 
-public class PlayerAnimationController : MonoBehaviour
+public class PlayerAnimationController : SingletonMonoBehavior<PlayerAnimationController>
 {
-    private Animator animator;
+    private Animator animator = null;
+    private List<SetPAnimator> pAnimatorsList = new List<SetPAnimator>();
+
+
     void Start()
     {
-        animator = this.GetComponent<Animator>();
+        // animator取得
+        animator = this.GetComponent<Animator>();   
+        // SetPAnimatorをList化
+        pAnimatorsList = Enum.GetValues(typeof(SetPAnimator)).Cast<SetPAnimator>().ToList();
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*Debug用
         if (Input.GetKeyDown(KeyCode.Z))
             SetAnimatorValue(SetPAnimator.Walk);
         if (Input.GetKeyDown(KeyCode.X))
@@ -31,33 +39,31 @@ public class PlayerAnimationController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.V))
             SetAnimatorValue(SetPAnimator.Push);
         if (Input.GetKeyDown(KeyCode.B))
-            SetAnimatorValue(SetPAnimator.Reset);
+            SetAnimatorValue(SetPAnimator.Stop);
+        */
+        
 
     }
-
-    private void SetAnimatorValue(SetPAnimator pAnimator)
+    /// <summary>
+    /// 指定されたAnimatorの値以外の変数を初期化(falseする 
+    /// </summary>
+    private void SetAnimatorValue(SetPAnimator param)
     {
-        switch (pAnimator)
+        //Debug.Log(param.ToString());
+        // animatorで遷移したい動きを先頭に持ってくる。
+        if (pAnimatorsList.IndexOf(param) > 0)
         {
-            case SetPAnimator.Walk:
-                animator.SetBool("Walk", true);
-                Debug.Log("a");
-                break;
-            case SetPAnimator.Run:
-                animator.SetBool("Run", true);
-                break;
-            case SetPAnimator.Hold:
-                animator.SetBool("Hold", true);
-                break;
-            case SetPAnimator.Push:
-                animator.SetBool("Push", true);
-                break;
-            case SetPAnimator.Reset:
-                animator.SetBool("Walk", false);
-                animator.SetBool("Run", false);
-                animator.SetBool("Hold", false);
-                animator.SetBool("Push", false);
-                break;
+            pAnimatorsList.RemoveAt(pAnimatorsList.IndexOf(param));
+            pAnimatorsList.Insert(0, param);
+        }
+        //Debug.Log(pAnimatorsList[0].ToString());
+        // 遷移したい動きにする。
+        animator.SetBool(param.ToString(), true);
+
+        // そのほかの動きをfalseにする
+        for(int i = 1; i < pAnimatorsList.Count; i++)
+        {
+            animator.SetBool(pAnimatorsList[i].ToString(), false);
         }
     }
 }
