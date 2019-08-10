ce7e7547aa03490c5164134ed39614061ef73e1d
smartwgy <smartwgy@gmail.com>
2019-08-11 03:20:13 +0900

    UIの位置調整

--- GhostCanGuard2019/Assets/Scripts/UI/GimmickUI.cs
+++ GhostCanGuard2019/Assets/Scripts/UI/GimmickUI.cs
@@ -7,14 +7,23 @@ public class GimmickUI : MonoBehaviour
 {
     private Vector3 parentGimmickPos = Vector3.zero;    // 親のGimmick格納
     private RectTransform rectTransformUI = null;       // UI自身のposition格納
-    [SerializeField] private Vector3 offset = new Vector3(-2.2f, 0, 2.2f);     // UIの配置調整
+    [SerializeField] private Vector3 plusoffset = new Vector3(2.2f, 0, 2.2f);     // UIの配置調整
+    [SerializeField] private Vector3 minusoffset = new Vector3(-10.2f, 0, 2.2f); //ギミックが画面の右にある時
 
 
     private void OnValidate()
     {
         parentGimmickPos = this.transform.root.position;    // 親のGimmick取得
         rectTransformUI = transform as RectTransform;       // UIのRectTransformを取得
-        rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + offset);
+        if (parentGimmickPos.x > 0)
+        {
+            rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + minusoffset);
+        }
+        else
+        {
+            rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + plusoffset);
+        }
+        
     }
     private void OnEnable()
     {
@@ -33,7 +42,14 @@ public class GimmickUI : MonoBehaviour
     {
         parentGimmickPos = this.transform.root.position;    // 親のGimmick取得
         rectTransformUI = transform as RectTransform;       // UIのRectTransformを取得
-        rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + offset);
+        if (parentGimmickPos.x > 0)
+        {
+            rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + minusoffset);
+        }
+        else
+        {
+            rectTransformUI.position = RectTransformUtility.WorldToScreenPoint(Camera.main, parentGimmickPos + plusoffset);
+        }
     }
   
 }
