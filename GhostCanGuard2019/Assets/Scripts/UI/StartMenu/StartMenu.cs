c7b0ad9c92fc7852d70450b018d9285353ffccb7
smartwgy <smartwgy@gmail.com>
2019-09-05 01:54:32 +0900

    タイトル画面ボタンのSprite変換

--- GhostCanGuard2019/Assets/Scripts/UI/StartMenu/StartMenu.cs
+++ GhostCanGuard2019/Assets/Scripts/UI/StartMenu/StartMenu.cs
@@ -71,21 +71,26 @@ public class StartMenu : MonoBehaviour
             if (Input.GetButtonDown("Send"))
             {
                 selectStartmenu.onClick.Invoke();
+                SwapSpriteState(selectStartmenu, ButtonState.Release);
                 changed = false;
             }
 
             if (Input.GetButtonDown("Right"))
             {
+                SwapSpriteState(selectStartmenu, ButtonState.None);
                 startmenuIndex = (startmenuIndex + 1) % startMenuButtons.Count;
                 selectStartmenu = startMenuButtons[startmenuIndex];
                 StartMenuSelectArray.rectTransform.position = selectStartmenu.GetComponent<RectTransform>().position - new Vector3(300, 0);
+                SwapSpriteState(selectStartmenu, ButtonState.Stay);
                 changed = true;
             }
             if (Input.GetButtonDown("Left"))
             {
+                SwapSpriteState(selectStartmenu, ButtonState.None);
                 startmenuIndex = (startmenuIndex + startMenuButtons.Count - 1) % startMenuButtons.Count;
                 selectStartmenu = startMenuButtons[startmenuIndex];
                 StartMenuSelectArray.rectTransform.position = selectStartmenu.GetComponent<RectTransform>().position - new Vector3(300, 0);
+                SwapSpriteState(selectStartmenu, ButtonState.Stay);
                 changed = true;
             }
             if (Input.GetAxisRaw("Right") != 0)
@@ -93,16 +98,20 @@ public class StartMenu : MonoBehaviour
                 if (changed) return;
                 if (Input.GetAxisRaw("Right") >= 0)
                 {
+                    SwapSpriteState(selectStartmenu, ButtonState.None);
                     startmenuIndex = (startmenuIndex + 1) % startMenuButtons.Count;
                     selectStartmenu = startMenuButtons[startmenuIndex];
                     StartMenuSelectArray.rectTransform.position = selectStartmenu.GetComponent<RectTransform>().position - new Vector3(300, 0);
+                    SwapSpriteState(selectStartmenu, ButtonState.Stay);
                     changed = true;
                 }
                 else
                 {
+                    SwapSpriteState(selectStartmenu, ButtonState.None);
                     startmenuIndex = (startmenuIndex + startMenuButtons.Count - 1) % startMenuButtons.Count;
                     selectStartmenu = startMenuButtons[startmenuIndex];
                     StartMenuSelectArray.rectTransform.position = selectStartmenu.GetComponent<RectTransform>().position - new Vector3(300, 0);
+                    SwapSpriteState(selectStartmenu, ButtonState.Stay);
                     changed = true;
                 }
             }
@@ -117,37 +126,47 @@ public class StartMenu : MonoBehaviour
             if (Input.GetButtonDown("Send"))
             {
                 selectStage.onClick.Invoke();
+                SwapSpriteState(selectStage, ButtonState.Release);
             }
             if (Input.GetButtonDown("Cancel"))
             {
                 cancelSelect();
+                SwapSpriteState(selectStage, ButtonState.None);
             }
             if (Input.GetButtonDown("Down"))
             {
+                SwapSpriteState(selectStage, ButtonState.None);
                 selectstageIndex = (selectstageIndex + 1) % stageButtons.Count;
                 selectStage = stageButtons[selectstageIndex];
                 StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                SwapSpriteState(selectStage, ButtonState.Stay);
                 changed = true;
             }
             if (Input.GetButtonDown("Up"))
             {
+                SwapSpriteState(selectStage, ButtonState.None);
                 selectstageIndex = (selectstageIndex + stageButtons.Count - 1) % stageButtons.Count;
                 selectStage = stageButtons[selectstageIndex];
                 StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                SwapSpriteState(selectStage, ButtonState.Stay);
                 changed = true;
             }
             if (Input.GetButtonDown("Right"))
             {
+                SwapSpriteState(selectStage, ButtonState.None);
                 selectstageIndex = (selectstageIndex + 4) % stageButtons.Count;
                 selectStage = stageButtons[selectstageIndex];
                 StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                SwapSpriteState(selectStage, ButtonState.Stay);
                 changed = true;
             }
             if (Input.GetButtonDown("Left"))
             {
+                SwapSpriteState(selectStage, ButtonState.None);
                 selectstageIndex = (selectstageIndex + stageButtons.Count - 4) % stageButtons.Count;
                 selectStage = stageButtons[selectstageIndex];
                 StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                SwapSpriteState(selectStage, ButtonState.Stay);
                 changed = true;
             }
 
@@ -157,16 +176,20 @@ public class StartMenu : MonoBehaviour
                 if (changed) return;
                 if (Input.GetAxisRaw("Down") <= 0)
                 {
+                    SwapSpriteState(selectStage, ButtonState.None);
                     selectstageIndex = (selectstageIndex + 1) % stageButtons.Count;
                     selectStage = stageButtons[selectstageIndex];
                     StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                    SwapSpriteState(selectStage, ButtonState.Stay);
                     changed = true;
                 }
                 else
                 {
+                    SwapSpriteState(selectStage, ButtonState.None);
                     selectstageIndex = (selectstageIndex + stageButtons.Count - 1) % stageButtons.Count;
                     selectStage = stageButtons[selectstageIndex];
                     StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                    SwapSpriteState(selectStage, ButtonState.Stay);
                     changed = true;
                 }
             }
@@ -175,16 +198,20 @@ public class StartMenu : MonoBehaviour
                 if (changed) return;
                 if (Input.GetAxisRaw("Right") >= 0)
                 {
+                    SwapSpriteState(selectStage, ButtonState.None);
                     selectstageIndex = (selectstageIndex + 4) % stageButtons.Count;
                     selectStage = stageButtons[selectstageIndex];
                     StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                    SwapSpriteState(selectStage, ButtonState.Stay);
                     changed = true;
                 }
                 else
                 {
+                    SwapSpriteState(selectStage, ButtonState.None);
                     selectstageIndex = (selectstageIndex + stageButtons.Count - 4) % stageButtons.Count;
                     selectStage = stageButtons[selectstageIndex];
                     StageSelectArray.rectTransform.position = selectStage.GetComponent<RectTransform>().position - new Vector3(200, 0);
+                    SwapSpriteState(selectStage, ButtonState.Stay);
                     changed = true;
                 }
             }
@@ -218,9 +245,15 @@ public class StartMenu : MonoBehaviour
         yield return StartCoroutine(startfadeout);
         selectstageOpen = true;
         selectStages.enabled = true;
+        SwapSpriteState(selectStartmenu, ButtonState.None);
     }
 
-
+    /// <summary>
+    /// フェードアウト
+    /// </summary>
+    /// <param name="canvas"></param>
+    /// <param name="fadeout_time"></param>
+    /// <returns></returns>
     IEnumerator fadeout(CanvasGroup canvas,float fadeout_time)
     {
         float dealpha = canvas.alpha / fadeout_time * Time.smoothDeltaTime;
@@ -238,28 +271,53 @@ public class StartMenu : MonoBehaviour
         }
     }
 
-    void selectNextButton(Button selector,int buttonIndex,List<Button> buttons)
-    {
-        buttonIndex = (buttonIndex + 1) % buttons.Count;
-        selector = buttons[buttonIndex];
-    }
-    void selectLastButton(Button selector, int buttonIndex, List<Button> buttons)
-    {
-        buttonIndex = (buttonIndex + buttons.Count - 1) % buttons.Count;
-        selector = buttons[buttonIndex];
-    }
-    void invokeStages(string stageName)
-    {
-        stagesMap[stageName].onClick.Invoke();
-    }
-    void invokeStartMenus(string startMenuButtonName)
-    {
-        startMenusMap[startMenuButtonName].onClick.Invoke();
-    }
+    //void selectNextButton(Button selector,int buttonIndex,List<Button> buttons)
+    //{
+    //    buttonIndex = (buttonIndex + 1) % buttons.Count;
+    //    selector = buttons[buttonIndex];
+    //}
+    //void selectLastButton(Button selector, int buttonIndex, List<Button> buttons)
+    //{
+    //    buttonIndex = (buttonIndex + buttons.Count - 1) % buttons.Count;
+    //    selector = buttons[buttonIndex];
+    //}
+    //void invokeStages(string stageName)
+    //{
+    //    stagesMap[stageName].onClick.Invoke();
+    //}
+    //void invokeStartMenus(string startMenuButtonName)
+    //{
+    //    startMenusMap[startMenuButtonName].onClick.Invoke();
+    //}
 
+    /// <summary>
+    /// ステージをロードする
+    /// </summary>
+    /// <param name="stageName"></param>
     public void LoadStage(string stageName)
     {
         LoadScene.loadScene(stageName);
     }
 
+    public void SwapSpriteState(Button button, ButtonState buttonState)
+    {
+        switch (buttonState)
+        {
+            //デフォルト
+            case ButtonState.None:
+                button.GetComponent<Image>().sprite = button.spriteState.disabledSprite;
+                break;
+            //選択されて
+            case ButtonState.Stay:
+                button.GetComponent<Image>().sprite = button.spriteState.highlightedSprite;
+                break;
+            //押されて
+            case ButtonState.Release:
+                button.GetComponent<Image>().sprite = button.spriteState.pressedSprite;
+                break;
+            default:
+                break;
+        }
+        
+    }
 }
