7a65a367e28eecb91874687c934436bcdbd2fd82
smartwgy <smartwgy@gmail.com>
2019-09-05 21:33:20 +0900

    説明文の追加

---
+++ GhostCanGuard2019/Assets/Scripts/UI/LoadImageFromFile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class LoadImageFromFile
+{
+    Sprite sprite;
+    
+    public Sprite GetSprite()
+    {
+        return sprite;
+    }
+
+    public void loadImageByName(string FileName)
+    {
+        sprite = Resources.Load<Sprite>("Senario/" + FileName);
+    }
+}
