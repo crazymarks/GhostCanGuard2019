7c00f27a3864e03e46d8acb9d1602337bd1c34c2
smartwgy <smartwgy@gmail.com>
2019-09-09 14:45:58 +0900

    馬のテキスチャー更新 VideoSceneを追加

---
+++ GhostCanGuard2019/Assets/Resources/Video/VideoSceneController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoSceneController : MonoBehaviour
+{
+    public videocontroller video;
+    bool haveLoad = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        haveLoad = false;
+        video.videoPlayer.targetCameraAlpha = 1;
+        video.videoPlayer.SetDirectAudioVolume(0, video.videoPlayer.targetCameraAlpha);
+        video.stopVideo();
+        video.playVideo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (haveLoad) return;
+        if (Input.anyKeyDown || !video.videoPlayer.isPlaying)
+        {
+            haveLoad = true;
+            StartCoroutine(fadeOut());
+        }
+
+    }
+
+    IEnumerator fadeOut()
+    {
+        while (true)
+        {
+            video.videoPlayer.targetCameraAlpha *= 0.96f;
+            video.videoPlayer.SetDirectAudioVolume(0, video.videoPlayer.targetCameraAlpha);
+            yield return new WaitForEndOfFrame();
+            if (video.videoPlayer.targetCameraAlpha < 0.05f)
+            {
+                video.stopVideo();
+                LoadScene.loadScene("Cp1");
+                Destroy(gameObject);
+                yield break;
+            }
+        }
+        
+    }
+}
---
+++ GhostCanGuard2019/Assets/Resources/Video/videocontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videocontroller : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    
+    public void playVideo()
+    {
+        videoPlayer.Play();
+    }
+    public void pauseVideo()
+    {
+        videoPlayer.Pause();
+    }
+    public void stopVideo()
+    {
+        videoPlayer.Stop();
+    }
+    public void reStartVideo()
+    {
+        videoPlayer.Stop();
+        videoPlayer.Play();
+    }
+}
